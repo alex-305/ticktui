b2cda2c99ab4ca1c2e14152a0f4d366aee2f7f7e
Alex Gonzalez <mail@alexgonz.dev>
2026-04-14 23:56:07 -0400

    moved token stuff into its own file

--- internal/config/config.go
+++ internal/config/config.go
@@ -63,27 +63,3 @@ func Save(cfg *Config) error {
 	viper.Set("default_project_color", cfg.DefaultProjectColor)
 	return viper.WriteConfigAs(configPath)
 }
-
-func LoadToken() (string, error) {
-	data, err := os.ReadFile(tokenPath)
-	if err != nil {
-		return "", err
-	}
-
-	return string(data), nil
-}
-
-func SaveToken(token string) error {
-	if err := os.MkdirAll(filepath.Dir(tokenPath), 0700); err != nil {
-		return errors.Wrap(err, "creating token directory")
-	}
-
-	return os.WriteFile(tokenPath, []byte(token), 0600)
-}
-
-func DeleteToken() error {
-	if err := os.Remove(tokenPath); err != nil && !os.IsNotExist(err) {
-		return err
-	}
-	return nil
-}
